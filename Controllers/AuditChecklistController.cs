25be48bf1a03cbbd14e19b6cc3664ba7f0271870
aathirams <aathisrikumar@outlook.com>
2021-10-05 13:13:12 +0530

    Add project files.

---
+++ Controllers/AuditChecklistController.cs
@@ -0,0 +1,52 @@
+﻿using AuditChecklistModule.Models;
+using AuditChecklistModule.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditChecklistModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AuditChecklistController : ControllerBase
+    {
+
+        private IChecklistService checklistServiceObj;
+        private readonly log4net.ILog log4netval;
+
+        public AuditChecklistController(IChecklistService checklistServiceObj)
+        {
+            log4netval = log4net.LogManager.GetLogger(typeof(AuditChecklistController));
+            this.checklistServiceObj = checklistServiceObj;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuditCheckListQuestions([FromBody] string auditType)
+        {
+            log4netval.Info("AuditChecklistController Http GET request called" + nameof(AuditChecklistController));
+
+            if (string.IsNullOrEmpty(auditType))
+                return BadRequest("No Input");
+            else if ((auditType != "Internal") && (auditType != "SOX"))
+                return Ok("Wrong Input");
+            else
+            {
+                try
+                {
+                    List<Questions> list = checklistServiceObj.GetQuestionList(auditType);
+                    return Ok(list);
+                }
+                catch (Exception e)
+                {
+                    log4netval.Error("Exception " + e.Message + nameof(AuditChecklistController));
+
+                    return StatusCode(500);
+                }
+            }
+        }
+    }
+}
---
+++ Services/ChecklistService.cs
@@ -0,0 +1,28 @@
+﻿using AuditChecklistModule.Models;
+using AuditChecklistModule.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditChecklistModule.Services
+{
+    public class ChecklistService : IChecklistService
+    {
+        private IChecklistRepo checklistRepoObj;
+        private log4net.ILog log4netval;
+        public ChecklistService(IChecklistRepo checklistRepoObj)
+        {
+            this.checklistRepoObj = checklistRepoObj;
+            log4netval = log4net.LogManager.GetLogger(typeof(ChecklistService));
+        }
+        List<Questions> ListOfQuestions = new List<Questions>();
+
+        public List<Questions> GetQuestionList(string auditType)
+        {
+            log4netval.Info(" Http GET request called" + nameof(ChecklistService));
+            ListOfQuestions = checklistRepoObj.GetQuestions(auditType);
+            return ListOfQuestions;
+        }
+    }
+}
